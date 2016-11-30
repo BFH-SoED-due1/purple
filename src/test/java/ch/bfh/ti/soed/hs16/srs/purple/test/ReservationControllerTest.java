439f9eef5a612c13f953d188a17b842be4c281a2
PA2-Laptop <PA2-Laptop@DESKTOP-9V08B7K.bfh.ch>
2016-11-30 14:52:10 +0100

    -created ReservationController and its tests

--- src/test/java/ch/bfh/ti/soed/hs16/srs/purple/test/ReservationControllerTest.java
+++ src/test/java/ch/bfh/ti/soed/hs16/srs/purple/test/ReservationControllerTest.java
@@ -8,19 +8,45 @@
 package ch.bfh.ti.soed.hs16.srs.purple.test;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import ch.bfh.ti.soed.hs16.srs.purple.controller.DBController;
+import ch.bfh.ti.soed.hs16.srs.purple.controller.DBController.Table_Room;
+import ch.bfh.ti.soed.hs16.srs.purple.controller.DBController.Table_User;
 import ch.bfh.ti.soed.hs16.srs.purple.controller.ReservationController;
+import ch.bfh.ti.soed.hs16.srs.purple.model.Room;
+import ch.bfh.ti.soed.hs16.srs.purple.model.User;
 
 public class ReservationControllerTest
 {
 
+	/*
 	@Test
 	public void RCTest()
 	{
 		ReservationController rc = new ReservationController();
 		assertNotNull(rc);
+	}*/
+	
+	static ReservationController reservationController;
+	
+	@BeforeClass
+	public static void testReservationController(){
+		User aebip1 = DBController.getInstance().selectUserBy(Table_User.COLUMN_USERNAME, "aebip1").get(0);
+		reservationController = new ReservationController(aebip1);
+		assertNotNull(reservationController);
+	}
+	
+	@Test
+	public void testAddReservation(){
+		Room room10 = DBController.getInstance().selectRoomBy(Table_Room.COLUMN_ROOMNUMBER, 10).get(0);
+		assertTrue(reservationController.addReservation(new Timestamp(new Date().getTime()), new Timestamp(new Date().getTime()+3600000), room10, "test add Reservation", "Reservation Added!", DBController.getInstance().selectUserBy(Table_User.COLUMN_USERNAME, "gestl1")));
 	}
 
 }
